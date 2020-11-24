0d74b97d8df41d9683451b6817c40c9b15694c4a
NicolasCalvet <nicolas.calvetgp@gmail.com>
2020-11-24 11:21:45 +0100

    Changed texture image, now texture works just fine on a single quad

--- source/main.cpp
+++ source/main.cpp
@@ -229,7 +229,7 @@ int main(void)
 
 
 	//Texture
-	const char *filename = "resources/images/tronc.jpg";
+	const char *filename = "resources/images/wall.jpg";
 	Image im = LoadImage(filename);
 
 	GLuint tex;
@@ -254,7 +254,7 @@ int main(void)
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
 
-		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
